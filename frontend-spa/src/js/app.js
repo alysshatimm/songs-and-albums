8b2a0f883e6b1b07873f015f1c057621037be2bb
Alyssha Timm <aly.nelso@gmail.com>
2020-11-11 10:42:34 -0500

    Modals on Modals

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -15,3 +15,39 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var modalAdd = document.getElementById("album-modal-add");
+var buttonAdd = document.getElementById("add-album-button");
+var spanAdd = document.getElementsByClassName("close-add")[0];
+
+buttonAdd.onclick = function() {
+    modalAdd.style.display = "block";
+}
+
+spanAdd.onclick = function() {
+    modalAdd.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modalAdd.style.display = "none";
+    }
+}
+
+var modalSong = document.getElementById("song-modal");
+var buttonSong = document.getElementById("song-button");
+var spanSong = document.getElementsByClassName("song-close")[0];
+
+buttonSong.onclick = function() {
+    modalSong.style.display = "block";
+}
+
+spanSong.onclick = function() {
+    modalSong.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modalSong.style.display = "none";
+    }
+}
