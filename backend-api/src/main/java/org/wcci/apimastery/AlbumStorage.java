0c2ab5fec5b06c4e38cd91431b8854d9e0455d7f
David Trimmer <dbtrimmer1@gmail.com>
2020-11-12 14:12:31 -0500

    It's time for the populator

--- backend-api/src/main/java/org/wcci/apimastery/AlbumStorage.java
+++ backend-api/src/main/java/org/wcci/apimastery/AlbumStorage.java
@@ -1,4 +1,7 @@
 package org.wcci.apimastery;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlbumStorage {
 }
--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -2,8 +2,29 @@ package org.wcci.apimastery;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.wcci.apimastery.storage.*;
 
 @Component
 public class Populator implements CommandLineRunner {
+
     private AlbumStorage albumStorage;
+    private SongRepository songRepo;
+    private AlbumCommentRepository albumCommentRepo;
+    private AlbumRatingRepository albumRatingRepo;
+    private SongCommentRepository songCommentRepo;
+    private SongRatingRepository songRatingRepo;
+
+    public Populator(AlbumStorage albumStorage, SongRepository songRepo, AlbumCommentRepository albumCommentRepo, AlbumRatingRepository albumRatingRepo, SongCommentRepository songCommentRepo, SongRatingRepository songRatingRepo) {
+        this.albumStorage = albumStorage;
+        this.songRepo = songRepo;
+        this.albumCommentRepo = albumCommentRepo;
+        this.albumRatingRepo = albumRatingRepo;
+        this.songCommentRepo = songCommentRepo;
+        this.songRatingRepo = songRatingRepo;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
