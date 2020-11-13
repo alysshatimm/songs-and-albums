a294a4116b1d4b5dbe4cb84af8947a3a476d290d
leviflint <tweekex@hotmail.com>
2020-11-13 15:08:16 -0500

    Cleaning up CSS and HTML

---
+++ frontend-spa/src/js/displaySingleAlbum.js
@@ -0,0 +1,7 @@
+import {allAlbums} from "./sampleAllAlbumsJson.js"
+
+const displaySingleAlbum = function (album) {
+    const mainContent = document.querySelector(`.album-button`);
+    clearChildren(mainContent);
+    
+}
