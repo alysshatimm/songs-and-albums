79cbd782d9275b60bd5ec8b46b8d29a62190cee3
leviflint <tweekex@hotmail.com>
2020-11-13 11:02:15 -0500

    Started Javascript

---
+++ frontend-spa/src/js/album.js
@@ -0,0 +1,30 @@
+const albumRating1 = document.getElementById(".song-star-1");
+const albumRating2 = document.getElementById(".song-star-2");
+const albumRating3 = document.getElementById(".song-star-3");
+const albumRating4 = document.getElementById(".song-star-4");
+const albumRating5 = document.getElementById(".song-star-5");
+
+albumRating1.addEventListener("click", () => {
+    
+})
+
+albumRating2.addEventListener("click", () => {
+    
+})
+
+albumRating3.addEventListener("click", () => {
+    
+})
+
+albumRating4.addEventListener("click", () => {
+    
+})
+
+albumRating5.addEventListener("click", () => {
+    
+})
+
+const averageAlbumRatingCalc  = function () {
+    let totalRatings = ${albumRating.size()}
+    let averageAlbumRating = ${albumRating.albumRating} / totalRatings;
+}
