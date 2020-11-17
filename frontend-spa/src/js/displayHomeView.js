974b1f10bccd1aec8ecb24d7065b368da16fb34b
Abigail McClaine <mcclaineabigail@gmail.com>
2020-11-17 15:39:21 -0500

    reformatted homescreen grid

--- frontend-spa/src/js/displayHomeView.js
+++ frontend-spa/src/js/displayHomeView.js
@@ -2,40 +2,40 @@ import { displaySingleAlbum } from "./displaySingleAlbum.js";
 import { allAlbums } from "./sampleAllAlbumsJson.js";
 
 
-const displayHomeView = function (albums) {
-  const mainElement = document.createElement("main");
-  mainElement.classList.add("container");
-  mainElement.classList.add("dropdown");
-  mainElement.classList.add("template-album");
-  mainElement.id = ("container")
+const displayHomeView = function(albums) {
+    const mainElement = document.getElementById("container");
+    // mainElement.classList.add("container");
+    // mainElement.classList.add("dropdown");
+    // mainElement.classList.add("template-album");
+    // mainElement.id = ("container")
 
-  // let campusListHtml = "";
+    // let campusListHtml = "";
 
-  albums.forEach((album) => {
-    let albumButton = document.createElement("button");
-    albumButton.classList.add("dropbtn");
-    albumButton.id = (`album-button${album.id}`)
-    let albumImage = document.createElement("img");
-    albumImage.classList.add("album-art");
-    albumImage.src = album.albumArt;
-    let albumArtist = document.createElement("h2");
-    albumArtist.classList.add("artist");
-    albumArtist.innerText = album.artist;
-    let albumName = document.createElement("h3");
-    albumName.classList.add("album-name");
-    albumName.innerText = album.albumTitle;
-    let recordLabel = document.createElement("h4");
-    recordLabel.classList.add("record-label");
-    recordLabel.innerText = album.recordLabel;
-    albumButton.appendChild(albumImage);
-    albumButton.appendChild(albumArtist);
-    albumButton.appendChild(albumName);
-    albumButton.appendChild(recordLabel);
-    mainElement.appendChild(albumButton);
-    
-  });
+    albums.forEach((album) => {
+        let albumButton = document.createElement("button");
+        albumButton.classList.add("dropbtn");
+        albumButton.id = (`album-button${album.id}`)
+        let albumImage = document.createElement("img");
+        albumImage.classList.add("album-art");
+        albumImage.src = album.albumArt;
+        let albumArtist = document.createElement("h2");
+        albumArtist.classList.add("artist");
+        albumArtist.innerText = album.artist;
+        let albumName = document.createElement("h3");
+        albumName.classList.add("album-name");
+        albumName.innerText = album.albumTitle;
+        let recordLabel = document.createElement("h4");
+        recordLabel.classList.add("record-label");
+        recordLabel.innerText = album.recordLabel;
+        albumButton.appendChild(albumImage);
+        albumButton.appendChild(albumArtist);
+        albumButton.appendChild(albumName);
+        albumButton.appendChild(recordLabel);
+        mainElement.appendChild(albumButton);
 
-  return mainElement;
+    });
+
+    return mainElement;
 };
 
 export { displayHomeView };
